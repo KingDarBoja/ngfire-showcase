c0fd82f9501f52bae96c1f8e9816de9b052a0a22
KingDarBoja <stevenbojato04@gmail.com>
2021-01-10 13:36:21 -0500

    chore: add ngneat/tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = (isProd) => ({
+    prefix: '',
+    purge: {
+      enabled: isProd,
+      content: [
+        './apps/**/*.{html,ts}',
+        './libs/**/*.{html,ts}',
+      ]
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+});
