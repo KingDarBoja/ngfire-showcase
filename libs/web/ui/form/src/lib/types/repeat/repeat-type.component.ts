1037b5f8f5602c34e95bda5946e7466c72270fe9
KingDarBoja <stevenbojato04@gmail.com>
2021-05-15 18:16:00 -0500

    fix: form repeat-type error while adding new entries
    Seems to be a fix for the "Expression has changed after it was checked" error.

--- libs/web/ui/form/src/lib/types/repeat/repeat-type.component.ts
+++ libs/web/ui/form/src/lib/types/repeat/repeat-type.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FieldArrayType } from '@ngx-formly/core';
 
 @Component({
@@ -11,15 +11,28 @@ import { FieldArrayType } from '@ngx-formly/core';
       <formly-field class="col-span-10" [field]="field"></formly-field>
       <div class="col-span-2">
         <button mat-mini-fab color="warn" type="button" (click)="remove(i)">
-          <mat-icon>delete_forever</mat-icon>
+          <mat-icon class="material-icons-outlined">delete_forever</mat-icon>
         </button>
       </div>
     </div>
     <section class="mt-4">
-      <button mat-stroked-button color="primary" type="button" (click)="add()">
+      <button mat-stroked-button color="primary" type="button" (click)="addEntry()">
         {{ to.addText }}
       </button>
     </section>
   `,
 })
-export class RepeatTypeComponent extends FieldArrayType {}
+export class RepeatTypeComponent extends FieldArrayType {
+  constructor(private readonly cdRef: ChangeDetectorRef) {
+    super();
+  }
+
+  addEntry(
+    i?: number,
+    initialModel?: never,
+    { markAsDirty } = { markAsDirty: true }
+  ) {
+    this.add(i, initialModel, { markAsDirty });
+    this.cdRef.detectChanges();
+  }
+}
