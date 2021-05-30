8ca1fcdc53b1a272d4bb36dd1e2bfbb62188771c
KingDarBoja <stevenbojato04@gmail.com>
2021-05-30 14:55:30 -0500

    feat: example of @angular/threejs with a sphere

--- libs/web/shell/feature/src/lib/dashboard/dashboard.component.ts
+++ libs/web/shell/feature/src/lib/dashboard/dashboard.component.ts
@@ -1,6 +1,55 @@
+import { CameraOptions } from '@angular-three/core';
 import { Component } from '@angular/core';
+import {
+  AxesHelper,
+  DoubleSide,
+  Mesh,
+  MeshBasicMaterialParameters,
+  SphereBufferGeometry,
+} from 'three';
 
 @Component({
-  template: ` <h2>Dashboard</h2> `,
+  styles: [
+    `
+      .globe-wrapper {
+        width: auto;
+        height: 400px;
+      }
+    `,
+  ],
+  template: `
+    <h2>Dashboard</h2>
+
+    <h4>ThreeJS Demo with @angular/threejs</h4>
+
+    <section class="globe-wrapper">
+      <ngt-canvas
+        [camera]="canvasCamera"
+        (created)="$event.gl.setClearColor('black')"
+      >
+        <ngt-axes-helper [args]="axesArgs"></ngt-axes-helper>
+        <ngt-mesh (animateReady)="animate($event.animateObject)">
+          <ngt-sphere-geometry [args]="sphereArgs"> </ngt-sphere-geometry>
+          <ngt-mesh-basic-material [parameters]="parameters">
+          </ngt-mesh-basic-material>
+        </ngt-mesh>
+      </ngt-canvas>
+    </section>
+  `,
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  canvasCamera: CameraOptions = {
+    position: [0, 0, 20],
+  };
+  parameters: MeshBasicMaterialParameters = {
+    color: '#009dff',
+    side: DoubleSide,
+    wireframe: true,
+  };
+  axesArgs: ConstructorParameters<typeof AxesHelper> = [100];
+  sphereArgs: ConstructorParameters<typeof SphereBufferGeometry> = [10, 40, 40];
+
+  animate(sphere: Mesh): void {
+    sphere.rotation.y += 0.01;
+  }
+}
--- libs/web/shell/feature/src/lib/dashboard/dashboard.module.ts
+++ libs/web/shell/feature/src/lib/dashboard/dashboard.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ThreeCoreModule } from '@angular-three/core';
+import { ThreeMeshModule } from '@angular-three/core/meshes';
+import { ThreeAxesHelperModule } from '@angular-three/core/helpers';
+import { ThreeSphereBufferGeometryModule } from '@angular-three/core/geometries';
+import { ThreeMeshBasicMaterialModule } from '@angular-three/core/materials';
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
+    ThreeCoreModule,
+    ThreeMeshModule,
+    ThreeAxesHelperModule,
+    ThreeSphereBufferGeometryModule,
+    ThreeMeshBasicMaterialModule,
     RouterModule.forChild([{ path: '', component: DashboardComponent }]),
   ],
 })
